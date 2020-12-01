0611c8f91b30aa8879bd518cc1d7c9c5ee3eee13
Sumit Bhagat <sumitbhagat@Sumits-MacBook-Air.local>
2020-12-01 22:49:13 +0530

    set up view engine

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ const port = 8000;
 // import routes
 app.use('/', require('./routes/index'));
 
+//setting view engine using app.set method
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
 
 app.listen(port, function(err){
     if(err){
