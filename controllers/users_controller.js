9dc81af7b2d6a02ddaae50a3ccdfc28d5a96ce9a
Sumit Bhagat <sumitbhagat@Sumits-MacBook-Air.local>
2020-12-02 17:33:14 +0530

    added ejs frwamework for views and connected to routes

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,5 +1,7 @@
 module.exports.profile = function(req, res){
-    return res.end('<h1>user profile</h1>');
+    return res.render('users', {
+        title :"Users/ profile"
+    });
 }
 
 module.exports.address = function(req, res){
