8af4b50bfadc5600b67ad5c9389a6730ee9a16df
Sumit Bhagat <sumitbhagat@Sumits-MacBook-Air.local>
2020-12-03 13:01:21 +0530

    created css file for individual pages, set up link extraction in index

--- index.js
+++ index.js
@@ -3,8 +3,12 @@ const app = express();
 const port = 8000;
 const expressLayouts = require('express-ejs-layouts');
 
-app.use(expressLayouts);
+app.use(express.static('./assets'));
 
+app.use(expressLayouts);
+// extract script and stylesheet from subpages to layout
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
 
 // import routes
 app.use('/', require('./routes/index'));
