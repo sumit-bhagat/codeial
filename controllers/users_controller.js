e9b8f2a1ac7f385d771a6ec88a0d2dcbef6cb124
Sumit Bhagat <sumitbhagat@Sumits-MacBook-Air.local>
2020-12-04 12:53:43 +0530

    Sign up Added- Getting problem with redirection after filling sign uo form

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,5 @@
+const User = require('../models/user');
+
 module.exports.profile = function(req, res){
     return res.render('users', {
         title :"Users/ profile"
@@ -16,4 +18,31 @@ module.exports.signIn = function(req, res){
     return res.render('user_sign_in', {
         title: "Codeial | Sign In "
     });
+}
+
+
+// get the sign up data
+module.exports.create = function(req, res){
+    if (req.body.password != req.body.confirm_password){
+        return res.redirect('back');
+    }
+
+    User.findOne({email : req.body.email}, function(err, user){
+        if(err){console.log('Error in finding user in signin up'); return}
+
+
+        if(!user){
+            User.create(req.body, function(err, user){
+                if(err){console.log('Error in finding user in signin up'); return}
+
+                return res.redirect('users/sign-in');
+            })
+        } else{
+            return res.redirect('back');
+        }
+    })
+}
+
+module.exports.createSession = function(req, res){
+    // TO DO connecting to route
 }
